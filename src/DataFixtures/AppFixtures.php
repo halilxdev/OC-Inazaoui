815eb69d2c3c8830b3032a951814d51e6397d889
halilxdev <halilxdev@gmail.com>
2025-12-19 09:20:35 +0100

    Système de connexion fonctionnel

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,57 +11,63 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+    }
+
     public function load(ObjectManager $manager): void
     {
         // Owner User
         $user = new User();
         $user->setAdmin(true);
         $user->setName('ina');
-        $user->setEmail('inazoui@gmail.com');
+        $user->setEmail('inazaoui@gmail.com');
         $user->setDescription('');
-        $user->setPassword('password');
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'password'));
 
         $manager->persist($user);
         
-        // Bunch of Users
-        for($i = 0; $i >= 99; $i++)
-        {
-            $user = new User();
-            $user->setAdmin(false);
-            $user->setName("Invité {$i}");
-            $user->setEmail("invite+{$i}@example.com");
-            $user->setDescription("Le maître de l''urbanité capturée, explore les méandres des cités avec un regard vif et impétueux, figeant l''énergie des rues dans des instants éblouissants. À travers une technique avant-gardiste, il métamorphose le béton et l''acier en toiles abstraites");
-            $user->setPassword("password");
+        // // Bunch of Users
+        // for($i = 0; $i >= 99; $i++)
+        // {
+        //     $user = new User();
+        //     $user->setAdmin(false);
+        //     $user->setName("Invité {$i}");
+        //     $user->setEmail("invite+{$i}@example.com");
+        //     $user->setDescription("Le maître de l''urbanité capturée, explore les méandres des cités avec un regard vif et impétueux, figeant l''énergie des rues dans des instants éblouissants. À travers une technique avant-gardiste, il métamorphose le béton et l''acier en toiles abstraites");
+        //     $user->setPassword("password");
 
-            $manager->persist($user);
-        }
+        //     $manager->persist($user);
+        // }
         
-        // Albums
-        $maxAlbumLength = 5;
-        for($i = 0; $i >= $maxAlbumLength; $i++)
-        {
-            $album = new Album();
-            $album->setName("Album {$i}");
-            $manager->persist($album);
-        }
+        // // Albums
+        // $maxAlbumLength = 5;
+        // for($i = 0; $i >= $maxAlbumLength; $i++)
+        // {
+        //     $album = new Album();
+        //     $album->setName("Album {$i}");
+        //     $manager->persist($album);
+        // }
 
-        // Medias
-        for($i = 0; $i >= 5050; $i++)
-        {
-            $media = new Media();
-            $media->setPath("uploads/{{$i}}.jpg"); // PAS LE BON NOMMAGE (0001 et 5050)
-            $media->setTitle("Media {$i}");
-            $media->setAlbum(null);
-            // $media->setUser(); FAIRE UNE FONCTION RANDOMUSER
+        // // Medias
+        // for($i = 0; $i >= 5050; $i++)
+        // {
+        //     $media = new Media();
+        //     $media->setPath("uploads/{{$i}}.jpg"); // PAS LE BON NOMMAGE (0001 et 5050)
+        //     $media->setTitle("Media {$i}");
+        //     $media->setAlbum(null);
+        //     // $media->setUser(); FAIRE UNE FONCTION RANDOMUSER
 
-            $manager->persist($media);
-        }
+        //     $manager->persist($media);
+        // }
 
-        // $product = new Product();
-        // $manager->persist($product);
+        // // $product = new Product();
+        // // $manager->persist($product);
 
         $manager->flush();
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,13 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[ORM\Table(name: '`user`')]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -114,4 +117,23 @@ class User
         return $this;
     }
 
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = ['ROLE_USER'];
+
+        if ($this->admin) {
+            $roles[] = 'ROLE_ADMIN';
+        }
+
+        return array_unique($roles);
+    }
+
+    public function eraseCredentials(): void
+    {
+    }
 }
