cedf1b1b38ac950ceab722251ac7320092f0f4ea
halilxdev <halilxdev@gmail.com>
2026-01-28 13:59:10 +0100

    Gestion des accès entre utilisateurs et admins

---
+++ src/Controller/Admin/GuestController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Media;
+use App\Form\MediaType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
+class GuestController extends AbstractController
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ){}
+
+    #[Route(path: '/admin/guests', name: 'admin_guest_index')]
+    public function index(Request $request)
+    {
+        $page = $request->query->getInt('page', 1);
+
+        $criteria = [];
+
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            $criteria['user'] = $this->getUser();
+        }
+
+        $medias = $this->entityManager->getRepository(Media::class)->findBy(
+            $criteria,
+            ['id' => 'ASC'],
+            25,
+            25 * ($page - 1)
+        );
+        $total = $this->entityManager->getRepository(Media::class)->count([]);
+
+        return $this->render('admin/media/index.html.twig', [
+            'medias' => $medias,
+            'total' => $total,
+            'page' => $page
+        ]);
+    }
+
+    #[Route(path: '/admin/guests/add', name: 'admin_media_add')]
+    public function add(Request $request)
+    {
+        $media = new Media();
+        $form = $this->createForm(MediaType::class, $media, ['is_admin' => $this->isGranted('ROLE_ADMIN')]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$this->isGranted('ROLE_ADMIN')) {
+                $media->setUser($this->getUser());
+            }
+            $media->setPath('uploads/' . md5(uniqid()) . '.' . $media->getFile()->guessExtension());
+            $media->getFile()->move('uploads/', $media->getPath());
+            $this->entityManager->persist($media);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('admin_media_index');
+        }
+
+        return $this->render('admin/media/add.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route(path: '/admin/guests/delete/{id}', name: 'admin_media_delete')]
+    public function delete(int $id)
+    {
+        $media = $this->entityManager->getRepository(Media::class)->find($id);
+        $this->entityManager->remove($media);
+        $this->entityManager->flush();
+        unlink($media->getPath());
+
+        return $this->redirectToRoute('admin_media_index');
+    }
+}
