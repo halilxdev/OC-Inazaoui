9610b83c58a73780976748508f608061679454ea
halilxdev <halilxdev@gmail.com>
2026-02-09 15:42:48 +0100

    Suppression des anciennes migrations

--- migrations/Version20260209144236.php
+++ migrations/Version20260209144236.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260209143919 extends AbstractMigration
+final class Version20260209144236 extends AbstractMigration
 {
     public function getDescription(): string
     {
